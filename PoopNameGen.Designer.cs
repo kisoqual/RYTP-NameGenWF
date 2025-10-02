70fc87458bb6ab3e86e5dd10d0f0270bc74806d0
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-10-03 00:52:42 +0300

    Add files via upload

---
+++ PoopNameGen.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RYTPNameGenWf
+{
+    partial class PoopNameGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "какой ты RYTP хочеш?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // confirm
+            // 
+            this.confirm.Location = new System.Drawing.Point(310, 53);
+            this.confirm.Name = "confirm";
+            this.confirm.Size = new System.Drawing.Size(105, 26);
+            this.confirm.TabIndex = 2;
+            this.confirm.Text = "Применить";
+            this.confirm.UseVisualStyleBackColor = true;
+            this.confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // PoopNameGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 86);
+            this.Controls.Add(this.confirm);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "PoopNameGen";
+            this.Text = "RYTP NameGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button confirm;
+    }
+}
+
---
+++ PoopNameGen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RYTPNameGenWf
+{
+    public partial class PoopNameGen : Form
+    {
+        public PoopNameGen()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + " RYTP";
+        }
+    }
+}
